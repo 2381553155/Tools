4046a8e24aae1074119bee188d7cbab945bd8286
WP <2381553155@qq.com>
2017-12-20 11:14:18 +0800

    Create Procedure.sql

---
+++ SQL/Procedure.sql
@@ -0,0 +1,17 @@
+drop procedure if exists selectWxpay;
+delimiter // 
+create procedure selectWxpay( 
+in  filed   varchar(50), 
+in  op    varchar(50), 
+in  filedVal  varchar(50)
+)
+begin 
+set @countL=10; 
+SET @strsql =concat('select * from s_wxpay_logs '," where ",filed,op,"'",filedVal,"'"); 
+-- 预定义一个语句，并将它赋给stmtsql 
+PREPARE stmtsql FROM @strsql; 
+EXECUTE stmtsql; 
+-- 释放一个预定义语句的资源 
+DEALLOCATE PREPARE stmtsql;
+end //delimiter ;
+call selectWxpay('openid','=','oj9EwwDPJVPKnuk_68d_FEspFe8M');
